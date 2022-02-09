29ada2696931b5c4ba4adfc54db8c071fe53d669
Kyulim Baek <kbaek@c4r2s3.42seoul.kr>
2022-02-09 16:21:41 +0900

    in process

--- proto.c
+++ proto.c
@@ -2,14 +2,15 @@
 
 int	main(int argc, char *argv[], char *env[])
 {
-	int		temp_argc;
-	char	**temp_argv;
-	char	**temp_env;
+	// int		temp_argc;
+	// char	**temp_argv;
+	// char	**temp_env;
 	char	*full_cmd;
 
-	temp_argc = argc;
-	temp_argv = argv;
-	temp_env = env;
+	// temp_argc = argc;
+	// temp_argv = argv;
+	// temp_env = env;
+	signal(SIGINT, interrupt);
 	while (1)
 	{
 		full_cmd = readline("> ");
