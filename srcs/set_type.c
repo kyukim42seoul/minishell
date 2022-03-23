473f6fc9898cb1f10ec3cfdf32fab6422f463c59
Kyungyoel Kim <kyukim@c7r2s8.42seoul.kr>
2022-03-23 21:28:34 +0900

    FIX : add cmd_flag in set_type() for cmd after redir

--- srcs/set_type.c
+++ srcs/set_type.c
@@ -2,8 +2,12 @@
 
 int	set_type(t_token *head)
 {
+	int	cmd_flag;
+	int	redir_flag;
 	t_token	*cur;
 
+	cmd_flag = 1;
+	redir_flag = 0;
 	cur = 0;
 	if (head == NULL)
 		return (EXIT_FAILURE);
@@ -11,10 +15,38 @@ int	set_type(t_token *head)
 	while (cur->next)
 	{
 		if (cur->prev->type == PIPE && cur->type == CHARACTERS)
+		{
 			cur->type = CMD;
+			cmd_flag = 0;
+		}
+		if (cur->type >= LEFT_REDI && cur->type <= RIGHT_DOUBLE_REDI)
+		{
+			cmd_flag = 0;
+			redir_flag = 1;
+		}
+		else if (redir_flag == 1 && cur->type == CHARACTERS)
+		{
+			redir_flag = 0;
+			cmd_flag = 1;
+		}
+		else if (cmd_flag == 1 && cur->type == CHARACTERS)
+		{
+			cur->type = CMD;
+			cmd_flag = 0;
+		}
+		if (cur->type == PIPE)
+			cmd_flag = 1;
+		if (cur->type == BUILTIN)
+			cmd_flag = 0;
+		printf("type data flag : %d %s %d %d\n", cur->type, cur->data, cmd_flag, redir_flag);
 		cur = cur->next;
 	}
 	if (cur->prev->type == PIPE && cur->type == CHARACTERS)
 		cur->type = CMD;
+	else if (cmd_flag == 1 && cur->type == CHARACTERS)
+	{
+		cur->type = CMD;
+		cmd_flag = 0;
+	}
 	return (EXIT_SUCCESS);
 }
