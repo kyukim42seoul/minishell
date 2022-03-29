58973084105dab4332549a36379774a66d7fdc74
Kyungyoel Kim <kyukim@c4r3s1.42seoul.kr>
2022-03-29 13:13:53 +0900

    FIX : solve leaks char *minishell

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: kyukim <kyukim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 19:09:24 by kyukim            #+#    #+#             */
-/*   Updated: 2022/03/28 21:20:49 by kyukim           ###   ########.fr       */
+/*   Updated: 2022/03/28 21:49:18 by kyukim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 
 int	init_info(t_info **info)
 {
+	char	*buf;
+
+	buf = getcwd(NULL, 0);
 	(*info) = malloc(sizeof(t_info));
 	(*info)->debug = (t_debug *)malloc(sizeof(t_debug));
 	(*info)->debug->syntax_error = 0;
 	(*info)->debug->error_point_data = 0;
 	(*info)->root = 0;
-	(*info)->minishell = ft_strjoin(getcwd(NULL, 0), "/minishell");
+	(*info)->minishell = ft_strjoin(buf, "/minishell");
+	free(buf);
 	return (EXIT_SUCCESS);
 }
